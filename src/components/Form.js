9c134311c6faaca747ad8f7199c892e95abacedd
dillonmd <dillonm.davis@yahoo.com>
2019-02-28 14:44:00 -0600

    Change Form to set number automatically

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,81 +1,79 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 // Sets initial state of form
 class Form extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.initialState = {
-            number: '',
-            location: '',
-            duration: '',
-            depth: '',
-            date: ''
-        };
+  constructor(props) {
+    super(props);
 
-        this.state = this.initialState;
-    }
+    this.initialState = {
+      number: 1,
+      location: "",
+      duration: "",
+      depth: "",
+      date: ""
+    };
 
-    handleChange = event => {
-        const { name, value } = event.target;
+    this.state = this.initialState;
+  }
 
-        this.setState({
-            [name] : value
-        });
-    }
-// Submits form
-    submitForm = () => {
-        this.props.handleSubmit(this.state);
-        this.setState(this.initialState);
-    }
-// Displays form 
-    render() {
-        const { number, location, duration, depth, date } = this.state; 
+  handleChange = event => {
+    const { name, value } = event.target;
 
-        return (
-            <form>
-                <label>Number</label>
-                <input 
-                    type="text" 
-                    name="number" 
-                    value={number} 
-                    onChange={this.handleChange} />
-                    <label>Date</label>
-                <input
-                    type="text"
-                    name="date"
-                    value={date}
-                    onChange={this.handleChange}/>
-                <label>Location</label>
-                <input 
-                    type="text" 
-                    name="location" 
-                    value={location} 
-                    onChange={this.handleChange}/>
-                    <label>Duration</label>
-                <input
-                    type="text"
-                    name="duration"
-                    value={duration}
-                    onChange={this.handleChange}/>
-                    <label>Depth</label>
-                <input
-                    type="text"
-                    name="depth"
-                    value={depth}
-                    onChange={this.handleChange}/>
-                <div
-                    className="submit-wrapper"
-                    name="wrapper">
-                <input 
-                    type="button" 
-                    value="Submit"
-                    className="submit-btn"
-                    onClick={this.submitForm} />
-                </div>
-            </form>
-        );
-    }
+    this.setState({
+      [name]: value
+    });
+  };
+  // Submits form
+  submitForm = () => {
+    this.props.handleSubmit(this.state);
+    this.setState(this.initialState);
+    this.initialState.number = this.initialState.number + 1;
+  };
+  // Displays form
+  render() {
+    const { number, location, duration, depth, date } = this.state;
+
+    return (
+      <form>
+        <label>Date</label>
+        <input
+          type="text"
+          name="date"
+          value={date}
+          onChange={this.handleChange}
+        />
+        <label>Location</label>
+        <input
+          type="text"
+          name="location"
+          value={location}
+          onChange={this.handleChange}
+        />
+        <label>Duration</label>
+        <input
+          type="text"
+          name="duration"
+          value={duration}
+          onChange={this.handleChange}
+        />
+        <label>Depth</label>
+        <input
+          type="text"
+          name="depth"
+          value={depth}
+          onChange={this.handleChange}
+        />
+        <div className="submit-wrapper" name="wrapper">
+          <input
+            type="button"
+            value="Submit"
+            className="submit-btn"
+            onClick={this.submitForm}
+          />
+        </div>
+      </form>
+    );
+  }
 }
 
 export default Form;
